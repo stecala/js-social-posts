2426cd347f0a2d05d8be569206b9d316e87e37d4
stecala <ste.cala123@gmail.com>
2022-06-30 13:38:57 +0200

    added the dynamic cards

--- main.js
+++ main.js
@@ -95,4 +95,41 @@ const posts = [
          "is_liked" : true,
         "created": "2021-12-11"
     }
-];
+]
+//* create const of the container of Post
+const postContainer = document.getElementById('container')
+
+//! we have to create the post in a cycle
+posts.forEach((element)=>{
+    const{id, content, media, author, likes, is_liked, created} = element
+    postContainer.innerHTML+=`<div class="post">
+                                <div class="post__header">
+                                    <div class="post-meta">
+                                        <div class="post-meta__icon">
+                                            <img class="profile-pic" src="${author.image}" alt="${author.name}">
+                                        </div>
+                                        <div class="post-meta__data">
+                                            <div class="post-meta__author">${author.name}</div>
+                                            <div class="post-meta__time">${created}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="post__text">${content}</div>
+                                <div class="post__image">
+                                    <img src="${media}" alt="">
+                                </div>
+                                <div class="post__footer">
+                                    <div class="likes js-likes">
+                                        <div class="likes__cta">
+                                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                <span class="like-button__label">Mi Piace</span>
+                                            </a>
+                                        </div>
+                                        <div class="likes__counter">
+                                            Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`
+})
